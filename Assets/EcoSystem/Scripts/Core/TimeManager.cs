2d7a3e34ae217a0faeedfa2f0dd593cc630f21a2
AndyCreighton87 <andrewcreighton87@gmail.com>
2025-06-18 14:36:24 +0100

    Years implementation

--- Assets/EcoSystem/Scripts/Core/TimeManager.cs
+++ Assets/EcoSystem/Scripts/Core/TimeManager.cs
@@ -5,10 +5,16 @@ public class TimeManager : MonoBehaviour {
 
     public static TimeManager Instance;
 
+    [SerializeField] private float secondsPerYear = 60.0f;
+
     public float TimeScale { get; private set; } = 1.0f;
 
     public float DeltaTime => Time.deltaTime * TimeScale;
 
+    private float accumulatedTime = 0f;
+    private float totalYearElapsed = 0f;
+
+    public event Action OnYearElapsed;
     public event Action<float> OnTimeScaleChanged;
 
     private void Awake() {
@@ -24,4 +30,18 @@ public class TimeManager : MonoBehaviour {
         TimeScale = value;
         OnTimeScaleChanged?.Invoke(TimeScale);
     }
+
+    private void Update() {
+        accumulatedTime += DeltaTime;
+
+        while (accumulatedTime >= secondsPerYear) {
+            accumulatedTime -= secondsPerYear;
+            totalYearElapsed += 1;
+            OnYearElapsed?.Invoke();
+        }
+
+        Debug.Log($"TotalYearsElapsed: {totalYearElapsed}");
+    }
+
+    public float GetYearFraction() => accumulatedTime / secondsPerYear;
 }
